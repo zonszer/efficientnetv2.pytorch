a44a9e37f3ac26c412fbee9e75b86409d2de7786
zonszer <zhangjiahan_1@163.com>
2022-05-16 21:53:19 +0800

    dataloader F1

--- dataset_define.py
+++ dataset_define.py
@@ -10,9 +10,7 @@ class MyData(Dataset):
     def __init__(self, root_dir,  label_dir, dataset_type, transform):
         self.root_dir = root_dir
         self.dataset_type = dataset_type 
-
         self.image_path = os.path.join(self.root_dir, self.dataset_type)
-
         self.image_list = os.listdir(self.image_path)  # 因为label 和 Image文件名相同，进行一样的排序，可以保证取出的数据和label是一一对应的
         # self.label_list = os.listdir(self.label_path)
 
@@ -26,11 +24,12 @@ class MyData(Dataset):
 
         # label_item_path = os.path.join(self.root_dir, self.label_dir, label_name)
         img = Image.open(img_item_path)
-        
         img = self.transform(img)
         
-        sample = {'img': img}
-        return sample
+        imgs = crop_image(img, 32, 32)      #大图crop into 小图
+
+        self.len = imgs.shape[0]
+        return imgs
 
     def __len__(self):
-        return len(self.image_list)
+        return self.len
